e2fc04f69bd3fe140ffbf72e072deb5aff3c62a5
sathish1980 <kumar.sathish189@gmail.com>
2023-05-25 09:30:17 +0530

    pull changes

--- src/main/java/sel/SeleniumTambaram9AMBatch/PropertyFile.java
+++ src/main/java/sel/SeleniumTambaram9AMBatch/PropertyFile.java
@@ -1,6 +1,12 @@
 package sel.SeleniumTambaram9AMBatch;
 
 public class PropertyFile {
+	
+	
+	public void newchanges()
+	{
+		System.out.println("Git pull concepts");
+	}
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
