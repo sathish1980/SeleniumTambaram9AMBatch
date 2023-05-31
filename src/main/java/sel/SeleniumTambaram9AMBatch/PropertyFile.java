1d4ead9d6b1201798b7cfedcbca9ff28e23ed46c
sathish1980 <kumar.sathish189@gmail.com>
2023-05-31 09:58:25 +0530

    first class

--- src/main/java/sel/SeleniumTambaram9AMBatch/PropertyFile.java
+++ src/main/java/sel/SeleniumTambaram9AMBatch/PropertyFile.java
@@ -1,16 +1,34 @@
 package sel.SeleniumTambaram9AMBatch;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.Properties;
+
 public class PropertyFile {
 	
-	
-	public void newchanges()
+	String fielpath ="C:\\Users\\sathishkumar\\eclipse-workspace\\SeleniumTambaram9AMBatch\\Input\\environment.properties";
+	public void proprtyFileHandling() throws IOException
 	{
-		System.out.println("Git pull concepts");
+		File F = new File(fielpath);
+		FileInputStream FI = new FileInputStream(F);
+		Properties p = new Properties();
+		p.load(FI);
+		String name = p.getProperty("name");
+		System.out.println(name);
+		String age = p.getProperty("age");
+		System.out.println(age);
+		String edu = p.getProperty("Edu");
+		System.out.println(edu);
+		p.setProperty("address", "chennai");
+		String  address= p.getProperty("address");
+		System.out.println(address);
 	}
-
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
 		// TODO Auto-generated method stub
-
+		PropertyFile P = new PropertyFile();
+		P.proprtyFileHandling();
 	}
 
 }
---
+++ src/test/java/SeleniumBasics/FirstClass.java
@@ -0,0 +1,38 @@
+package SeleniumBasics;
+
+import org.openqa.selenium.*;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+
+public class FirstClass {
+	
+	public void launch()
+	{
+		WebDriver browser = new EdgeDriver();
+		browser.get("https://www.facebook.com/");
+		browser.manage().window().maximize();
+		/*browser.navigate().to("https://www.gmail.com/");
+		// browser.manage().window().setPosition(200);
+		/*WebDriver browser1 = new ChromeDriver();
+		browser1.navigate().to("https://www.gmail.com/");*/
+		/*browser.navigate().back();
+		browser.navigate().forward();
+		browser.navigate().refresh();
+		browser.close();
+		browser.quit();*/
+		
+		WebElement username =browser.findElement(By.id("email"));
+		username.sendKeys("sathish");
+		// username.clear();
+		//or
+		browser.findElement(By.id("email")).sendKeys("kumar");
+		// browser.findElement(By.id("email")).clear();
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		FirstClass F = new FirstClass();
+		F.launch();
+	}
+
+}
